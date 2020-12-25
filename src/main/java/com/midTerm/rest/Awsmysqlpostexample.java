f99759a8e52c78954b8c16d742610410e16b17da
DESKTOP-DQ3SVL9\sid shah <siddharth9365@gmail.com>
2020-12-25 13:11:06 -0500

    Adding Update employee and Transaction from to todate services

--- src/main/java/com/midTerm/rest/Awsmysqlpostexample.java
+++ src/main/java/com/midTerm/rest/Awsmysqlpostexample.java
@@ -287,4 +287,106 @@ public class Awsmysqlpostexample
 		return Response.status(status).entity(mainobj.toString()).build();
 	}
 	
+	@POST
+	@Path("/updateEmp")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createEmployee(Customer customer) {
+		Sqlconnector connection = new Sqlconnector();
+
+		con = connection.getConnection();
+
+		int status = 200;
+		try {
+
+			String query = "update customer set ADDRESS = ? where CUST_ID = ?;";
+
+			preparedStatement = con.prepareStatement(query);
+
+			preparedStatement.setInt(2, customer.getId());
+			preparedStatement.setString(1, customer.getAddress());
+
+			int rowCount = preparedStatement.executeUpdate();
+
+			if (rowCount > 0) {
+				System.out.println("Record Update Successfully");
+
+				mainobj.accumulate("status", 200);
+				mainobj.accumulate("message", "Successfully Updated");
+			} else {
+				status = 500;
+				mainobj.accumulate("status", 500);
+				mainobj.accumulate("message", "Something Wrong");
+			}
+
+		} catch (SQLException e) {
+			status = 500;
+			mainobj.accumulate("status", 500);
+			mainobj.accumulate("message", e.getMessage());
+		} finally {
+			try {
+				con.close();
+				preparedStatement.close();
+			} catch (SQLException e) {
+				System.out.println("Finally Block SQL Exception : " + e.getMessage());
+			}
+		}
+		return Response.status(status).entity(mainobj.toString()).build();
+	}
+	
+	@GET
+	@Path("/getTransactions/{from}/{to}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getTransactions(@PathParam("from") String from,@PathParam("to") String to) 
+	{	
+		Sqlconnector connection = new Sqlconnector();	 
+		con = connection.getConnection();
+		
+		try 
+		{
+			stmt = con.createStatement();
+			String query = "select x.TXN_ID,x.AMOUNT,x.FUNDS_AVAIL_DATE,x.TXN_DATE,x.TXN_TYPE_CD,a.CUST_ID,i.FIRST_NAME,i.LAST_NAME from acc_transaction x,account a ,customer c,individual i where x.ACCOUNT_ID = a.ACCOUNT_ID and a.CUST_ID = c.CUST_ID and i.CUST_ID = c.CUST_ID and TXN_DATE between \""+from+"\" and \""+to+"\";";
+			rs = stmt.executeQuery(query);
+			System.out.println(query);
+			while(rs.next()) 
+			{
+				childObj = new JSONObject();
+				
+				childObj.accumulate("Trasaction ID", rs.getString("x.TXN_ID"));
+				childObj.accumulate("Amount", rs.getString("x.AMOUNT"));
+				childObj.accumulate("Funds Available Date", rs.getString("x.FUNDS_AVAIL_DATE"));
+				childObj.accumulate("Transaction Date", rs.getString("x.TXN_DATE"));
+				childObj.accumulate("Transaction Type Code", rs.getString("x.TXN_TYPE_CD"));
+				childObj.accumulate("Customer ID", rs.getString("a.CUST_ID"));
+				childObj.accumulate("First Name", rs.getString("i.FIRST_NAME"));
+				childObj.accumulate("Last Name", rs.getString("i.LAST_NAME"));
+				
+				
+				jsonArray.put(childObj);
+	
+			}
+			mainobj.put("Transactions",jsonArray);
+			//mainobj.put("individual", jsonArray);
+		}
+		catch(SQLException e) 
+		{
+			System.out.println("SQL Exception : "+e.getMessage());
+		}
+		finally 
+		{
+			try 
+			{
+				con.close();
+				stmt.close();
+				rs.close();
+			}
+			catch(SQLException e)
+			{
+				System.out.println("Finally Block SQL Exception : "+e.getMessage());
+			}
+		}
+		
+		return Response.status(200).entity(mainobj.toString()).build();	
+	}
+	
 }
