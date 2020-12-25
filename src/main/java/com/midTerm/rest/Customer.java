be3ec07e833939206c48f76db4bc94730d273009
DESKTOP-DQ3SVL9\sid shah <siddharth9365@gmail.com>
2020-12-25 13:07:53 -0500

    Adding Customer POJO

---
+++ src/main/java/com/midTerm/rest/Customer.java
@@ -0,0 +1,19 @@
+package com.midTerm.rest;
+
+public class Customer {
+	public int id;
+	public String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
